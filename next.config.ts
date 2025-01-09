9315c073bf714386bfa150033b78db85e7500a2c
Maurizio Carella <maurizio.carella27@gmail.com>
2025-01-10 00:02:51 +0100

    fix: fixed docker build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	output: 'standalone',
+	sassOptions: {
+		silenceDeprecations: ['legacy-js-api'],
+	},
 };
 
 export default nextConfig;
